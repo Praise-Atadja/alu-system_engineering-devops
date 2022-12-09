ec822e90b39a8d7eb14ea487b1bf85c146939c7a
Praise-Atadja <p.atadja@alustudent.com>
2022-12-09 18:45:23 +0000

    debugging 4 fix

--- web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -6,7 +6,7 @@ service {'nginx':
 exec {'increase ULIMIT':
   #replacae the value
   command  => 'sed -i "s/ULIMIT=\"-n 15\"/ULIMIT=\"-n 3000\"/g" /etc/default/nginx',
-  provider => 'shell'
+  provider => 'shell',
   #restart nginx
-  notify   => Service['nginx']
+  notify   => service['nginx']
 }
