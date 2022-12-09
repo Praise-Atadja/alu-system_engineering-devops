34b10343d29f71f551b1496dff6107bccf428a27
Praise-Atadja <p.atadja@alustudent.com>
2022-12-09 08:16:39 -0800

    puppet

--- web_stack_debugging_3/0-strace_is_your_friend.pp
+++ web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,5 @@
 #find out why Apache is returning a 500 error.
 exec { 'fix /var/www/html/wp-settings.php' :
     command  => 'sed -i "s/class-wp-locale.phpp/class-wp-locale.php/g" /var/www/html/wp-settings.php',
-provider => '/bin/bash',
+provider => 'shell',
 }
